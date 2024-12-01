9f83f1be205c031a60ab913c80057a5aa52b2067
Alex Ramsdell <alexramsdell@gmail.com>
2024-12-01 00:03:56 -0500

    support comments for lines starting with #, space ignored. support variable names

--- lang.py
+++ lang.py
@@ -1,7 +1,7 @@
-def eval_exp(line):
+def eval_expr(expr, line_no, names):
 
     stack = []
-    tokens = line.strip().split()
+    tokens = expr.strip().split()
 
     for token in tokens:
 
@@ -26,21 +26,43 @@ def eval_exp(line):
             stack.append(lhs * rhs)
 
         else:
-            stack.append(int(token))
+            if token.isalpha():
+                try:
+                    stack.append(names[token])
+                except KeyError:
+                    print(f"{line_no}: token not defined: {token}")
+                    exit(1)
+            else:
+                stack.append(int(token))
 
     return stack.pop()
 
 def eval(lines):
 
     pc = 0
+    names = {}
 
     while pc < len(lines):
 
         line = lines[pc]
-        v = eval_exp(line)
+
+        if len(line.split('=', maxsplit=1)) == 2:
+            name, expr = line.split('=', maxsplit=1)
+            names[name.strip()] = eval_expr(expr, pc, names)
+        else:
+            eval_expr(line_no, line, names)
 
         pc += 1
 
+    print(names)
+    
+
 if __name__ == '__main__':
-    f = [ line for line in open('main.alex').readlines() if line.strip() ]
+
+    f = [ 
+         line for line
+         in open('main.alex').readlines()
+         if len(line.strip()) 
+         and not line.strip().startswith('#') ]
+
     eval(f)
--- test_lang.py
+++ test_lang.py
@@ -1,4 +1,4 @@
 import lang
 
 def test_calcs():
-    assert lang.eval_exp("2 4 - 1 / 3 +") == 1 
+    assert lang.eval_expr("2 4 - 1 / 3 +") == 1 
