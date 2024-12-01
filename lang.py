ff0ae0db19081ec4c85fcb7ac769fed098bfaeb3
Alex Ramsdell <alexramsdell@gmail.com>
2024-11-30 23:13:31 -0500

    test rpn

--- lang.py
+++ lang.py
@@ -1,40 +1,46 @@
+def eval_exp(line):
+
+    stack = []
+    tokens = line.strip().split()
+
+    for token in tokens:
+
+        if token == '-':
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs - rhs)
+
+        elif token == '+':
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs + rhs)
+
+        elif token == '/':
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs / rhs)
+
+        elif token == '*':
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs * rhs)
+
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
+
 def eval(lines):
 
     pc = 0
 
     while pc < len(lines):
 
-        stack = []
-        tokens = lines[pc].strip().split()
+        line = lines[pc]
+        v = eval_exp(line)
 
-        for token in tokens:
-
-            if token == '-':
-                rhs = stack.pop()
-                lhs = stack.pop()
-                stack.append(lhs - rhs) 
-
-            elif token == '+':
-                rhs = stack.pop()
-                lhs = stack.pop()
-                stack.append(lhs + rhs) 
-
-            elif token == '/':
-                rhs = stack.pop()
-                lhs = stack.pop()
-                stack.append(lhs / rhs) 
-
-            elif token == '*':
-                rhs = stack.pop()
-                lhs = stack.pop()
-                stack.append(lhs * rhs) 
-
-            else:
-                stack.append(int(token))
-
-        print(stack.pop())
         pc += 1
 
-f = [ line for line in open('main.alex').readlines() if line.strip() ]
-
-eval(f)
+if __name__ == '__main__':
+    f = [ line for line in open('main.alex').readlines() if line.strip() ]
+    eval(f)
---
+++ test_lang.py
@@ -0,0 +1,4 @@
+import lang
+
+def test_calcs():
+    assert lang.eval_exp("2 4 - 1 / 3 +") == 1 
