fb6dbf33b83882a953a8110f7bf671646d49cdb7
Alex Ramsdell <alexramsdell@gmail.com>
2024-11-30 22:43:17 -0500

    rpn expressions evaluated.

--- lang.py
+++ lang.py
@@ -7,31 +7,34 @@ def eval(lines):
         stack = []
         tokens = lines[pc].strip().split()
 
-
         for token in tokens:
 
             if token == '-':
                 rhs = stack.pop()
                 lhs = stack.pop()
                 stack.append(lhs - rhs) 
+
             elif token == '+':
                 rhs = stack.pop()
                 lhs = stack.pop()
                 stack.append(lhs + rhs) 
+
             elif token == '/':
                 rhs = stack.pop()
                 lhs = stack.pop()
                 stack.append(lhs / rhs) 
+
             elif token == '*':
                 rhs = stack.pop()
                 lhs = stack.pop()
                 stack.append(lhs * rhs) 
+
             else:
                 stack.append(int(token))
 
         print(stack.pop())
         pc += 1
 
-f = open('main.alex').readlines()
+f = [ line for line in open('main.alex').readlines() if line.strip() ]
 
 eval(f)
